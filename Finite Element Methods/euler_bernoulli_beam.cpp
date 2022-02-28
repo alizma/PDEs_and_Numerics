34bd62a9901d97a691506ebad71692b401852140
Alexey Izmailov <alizma@brown.edu>
2022-02-28 02:27:36 -0500

    implemented reactive force computation for euler_bernoulli_beams

--- Finite Element Methods/euler_bernoulli_beam.cpp
+++ Finite Element Methods/euler_bernoulli_beam.cpp
@@ -6,7 +6,7 @@ std::vector<std::vector<std::vector<double>>> construct_local_stiffness() {
     std::vector<std::vector<std::vector<double>>> element_stiffness_matrices{};
     for (int curr_element = 0; curr_element < K; curr_element++) {
         double hk = HK[curr_element], constant = 6. * EI[curr_element] / (hk * hk); 
-        std::cout << hk << ' ' << constant << std::endl; 
+        // std::cout << hk << ' ' << constant << std::endl; 
 
         std::vector<std::vector<double>> curr_matrix = {
             {constant * 6. / hk, constant * 3.,  constant * -6. / hk, constant * 3.},
@@ -36,7 +36,7 @@ std::vector<std::vector<double>> construct_local_load() {
     return element_load_vectors; 
 }
 
-std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globals(std::vector<std::vector<std::vector<double>>>& local_stiffness_matrices, std::vector<std::vector<double>>& local_load_vectors){ 
+std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globals_raw(std::vector<std::vector<std::vector<double>>>& local_stiffness_matrices, std::vector<std::vector<double>>& local_load_vectors){
     // intialize NUM_NODESxNUM_NODES matrix with 0.'s 
     std::vector<std::vector<double>> global_stiffness(NUM_NODES, std::vector<double>(NUM_NODES));
 
@@ -53,8 +53,8 @@ std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globa
                 iglobal = ELDOF[curr_element][idx] - 1;
                 jglobal = ELDOF[curr_element][jdx] - 1; 
                 global_stiffness[iglobal][jglobal] += local_stiffness_matrices[curr_element][idx][jdx];  
-                std::cout << idx << ' ' << jdx << std::endl; 
-                std::cout << iglobal << ' ' <<  jglobal << ' ' << global_stiffness[iglobal][jglobal] << std::endl;  
+                //std::cout << idx << ' ' << jdx << std::endl; 
+                //std::cout << iglobal << ' ' <<  jglobal << ' ' << global_stiffness[iglobal][jglobal] << std::endl;  
             }
             global_loads[iglobal] += local_load_vectors[curr_element][idx]; 
         }
@@ -65,54 +65,49 @@ std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globa
         global_loads[std::get<0>(NEU_DOF[idx]) - 1] += std::get<1>(NEU_DOF[idx]);
     }
 
+    return std::make_pair(global_stiffness, global_loads);  
+}
+
+std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globals(std::pair<std::vector<std::vector<double>>, std::vector<double>>& raw_globals) {
     // apply Dirichlet constraints
     for (int idx = 0; idx < DIR_DOF.size(); idx++) { 
         int dirichlet_index = DIR_DOF[idx] - 1; 
 
         for (int row_idx = 0; row_idx < NUM_NODES; row_idx++) { 
-            global_stiffness[dirichlet_index][row_idx] = 0.;
-            global_stiffness[row_idx][dirichlet_index] = 0.; 
-            global_stiffness[dirichlet_index][dirichlet_index] = 1.;
+            std::get<0>(raw_globals)[dirichlet_index][row_idx] = 0.;
+            std::get<0>(raw_globals)[row_idx][dirichlet_index] = 0.; 
+            std::get<0>(raw_globals)[dirichlet_index][dirichlet_index] = 1.;
         }
 
-        global_loads[dirichlet_index] = 0.; 
+        std::get<1>(raw_globals)[dirichlet_index] = 0.; 
     }
 
-    return std::make_pair(global_stiffness, global_loads);  
+    return raw_globals; 
+}
+
+std::vector<double> compute_resultant_forces(std::pair<std::vector<double>, std::vector<double>>& raw_globals) {
+    int num_resultant_forces = RESULTANT_FORCE_NODES.size(); 
+    std::vector<double> resultant_forces(num_resultant_forces); 
+
+    for (int node_idx = 0; node_idx < num_resultant_forces; node_idx++) { 
+        int curr_node_num = RESULTANT_FORCE_NODES[node_idx] - 1; 
+        resultant_forces[curr_node_num] += std::get<0>(raw_globals)[curr_node_num] - std::get<1>(raw_globals)[curr_node_num];
+    }
+
+    return resultant_forces; 
 }
 
 int main() { 
     std::vector<std::vector<std::vector<double>>> element_stiffness_matrices = construct_local_stiffness(); 
     std::vector<std::vector<double>> element_load_vectors = construct_local_load(); 
-    std::pair<std::vector<std::vector<double>>, std::vector<double>> globals = construct_globals(element_stiffness_matrices, element_load_vectors); 
+    std::pair<std::vector<std::vector<double>>, std::vector<double>> raw_globals = construct_globals_raw(element_stiffness_matrices, element_load_vectors); 
+    std::pair<std::vector<std::vector<double>>, std::vector<double>> globals = construct_globals(raw_globals); 
 
-    for (int curr_element = 0; curr_element < 2; curr_element++) { 
-        for (int idx = 0; idx < 4; idx++) { 
-            std::cout << element_load_vectors[curr_element][idx] << ' '; 
-        }
-        std::cout << std::endl; 
-    }
-
-    for (int curr_element = 0; curr_element < 2; curr_element++) { 
-        for (int idx = 0; idx < 4; idx++) { 
-            for (int jdx = 0; jdx < 4; jdx++) { 
-                    std::cout << element_stiffness_matrices[curr_element][idx][jdx] << ' '; 
-            }
-            std::cout << std::endl; 
-        }
-        std::cout << std::endl;
-    }
-
+    std::cout << "==== GLOBAL LOAD VECTOR ===" << std::endl; 
     for (int idx = 0; idx < NUM_NODES; idx++) { 
         std::cout << std::get<1>(globals)[idx] << std::endl; 
     }
-
-    for (int idx = 0; idx < NUM_NODES; idx++) { 
-        for (int jdx = 0; jdx < NUM_NODES; jdx++) { 
-            std::cout << std::get<0>(globals)[idx][jdx] << ' ';    
-        }
-        std::cout << std::endl; 
-    }
+    std::cout << std::endl; 
 
     // note that the nodes that are shared by two adjacent elements are counted exactly 3 times
     for (int curr_element = 0; curr_element < K; curr_element++) { 
@@ -126,26 +121,42 @@ int main() {
         }
     }
 
+    std::cout << "=== GLOBAL STIFFNESS MATRIX ===" << std::endl; 
     for (int idx = 0; idx < NUM_NODES; idx++) { 
         for (int jdx = 0; jdx < NUM_NODES; jdx++) { 
             std::cout << std::get<0>(globals)[idx][jdx] << ' ';    
         }
         std::cout << std::endl; 
     }
+    std::cout << std::endl; 
 
     std::vector<std::vector<double>> inv_matrix = inverse(std::get<0>(globals));
 
+    std::cout << "=== INVERSE STIFFNESS MATRIX ===" << std::endl; 
     for (int idx = 0; idx < NUM_NODES; idx++) { 
         for (int jdx = 0; jdx < NUM_NODES; jdx++) { 
             std::cout << inv_matrix[idx][jdx] << ' ';    
         }
         std::cout << std::endl; 
     }
+    std::cout << std::endl;  
 
    std::vector<double> alpha_vec = matrix_times_vector(inv_matrix, std::get<1>(globals)); 
 
+    std::cout << "=== ALPHA VECTOR OF COEFFICIENTS ===" << std::endl; 
     for (int idx = 0; idx < alpha_vec.size(); idx++) { 
         std::cout << alpha_vec[idx] << std::endl; 
+    }
+    std::cout << std::endl; 
+
+    std::vector<double> s_raw_alpha = matrix_times_vector(std::get<0>(raw_globals), alpha_vec);
+    std::pair<std::vector<double>, std::vector<double>> raw_vectors = std::make_pair(s_raw_alpha, std::get<1>(raw_globals)); 
+
+    std::vector<double> resultant_forces = compute_resultant_forces(raw_vectors); 
+
+    std::cout << "=== RESULTANT FORCES AT FIXED POINTS OR PIVOTS ===" <<  std::endl;
+    for (int idx = 0; idx < resultant_forces.size(); idx++) { 
+        std::cout << resultant_forces[idx] << std::endl;
     }
 
     return 0; 
--- Finite Element Methods/euler_bernoulli_beam.h
+++ Finite Element Methods/euler_bernoulli_beam.h
@@ -3,26 +3,38 @@
 #include <utility> 
 #include <array> 
 
-
+/* DECLARATIONS OF PROBLEM-SPECIFIC STRUCTURES */ 
 // map from node number to x-coord - nodes are 1, 2, 3: 1 at LHS boundary, 2 at midpoint, 3 at RHS boundary 
-const std::vector<double> COORDS { 0, 0.5, 1 }; 
+const std::vector<double> COORDS { 5. / 2, 5., 7. }; 
 // map from element number to node number -- element I maps to nodes 1 & 2, element II maps to nodes 2 & 3
-const std::vector<std::pair<int, int>> ELNODE = { std::make_pair(1, 2), std::make_pair(2, 3) };
+const std::vector<std::pair<int, int>> ELNODE = { std::make_pair(1, 3), std::make_pair(3, 2), std::make_pair(2, 4) };
 // map from element number to numbers of degrees of freedom
-const std::vector<std::array<int, 4>> ELDOF = { {1, 4, 2, 5}, {2, 5, 3, 6} };  
+const std::vector<std::array<int, 4>> ELDOF = { {1, 5, 2, 6}, {2, 6, 4, 8}, {4, 8, 3, 7} };  
 // mapping from degree of freedom to essential boundary conditions (Dirichlet BCs)
-const std::vector<int> DIR_DOF = { 1, 4, 3, 6 };
+const std::vector<int> DIR_DOF = { 1, 5, 3 };
 // mapping from degrees of freedom to natural constraints (Neumann BCs)
-const std::vector<std::pair<int, double>> NEU_DOF; // in the 2-interval problem from Pset4, this is empty
+const std::vector<std::pair<int, double>> NEU_DOF = { std::make_pair(2, 5.), std::make_pair(4, 3.)}; // in the 2-interval problem from Pset4, this is empty
 // K - number of elements 
-const int K = 2; 
+const int K = 3; 
 // number of nodes on intervals of elements 
 const int NUM_NODES = 2*(K+1); 
 // fluxural rigidity of each element 
-const std::array<double, K> EI = { 1., 1. }; 
+const std::array<double, K> EI = { 1., 1., 1. }; 
 // step sizes between adjacent nodes 
-const std::array<double, K> HK = { 0.5, 0.5 };
+const std::array<double, K> HK = { 5. / 2, 5. / 2, 2. };
 // forces for each element segment 
-const std::array<double, K> FK = { 1., 1. };
+const std::array<double, K> FK = { -1., -1., -1. };
+// nodes to compute resultant forces at 
+const std::vector<int> RESULTANT_FORCE_NODES = { 1, 2 }; 
 
-// TODO: WRITE FUNCTION PROTOTYPE DECLARATIONS
+/* DECLARATIONS OF FEM FUNCTIONS */
+// computes local stiffness matrices and returns a vector of arrays
+std::vector<std::vector<std::vector<double>>> construct_local_stiffness(); 
+// computes local load vectors and returns a vector of vectors 
+std::vector<std::vector<double>> construct_local_load();
+// computes RAW global stiffness matrix and global load vector 
+std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globals_raw(std::vector<std::vector<std::vector<double>>>& , std::vector<std::vector<double>>& );
+// computes global stiffness matrix and global load vector accounting for Dirichlet BCs 
+std::pair<std::vector<std::vector<double>>, std::vector<double>> construct_globals(std::pair<std::vector<std::vector<double>>, std::vector<double>>& ); 
+// computes resultant forces at relevant nodes
+std::vector<double> compute_resultant_forces(std::pair<std::vector<double>, std::vector<double>>& ); 
